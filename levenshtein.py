17e6259df73530255e7b65ebec1eb423e17969be
MateusCristoMelo <mateuscristomelo@hotmail.com>
2022-12-06 21:31:44 -0300

    Faster leveinshtein

--- levenshtein.py
+++ levenshtein.py
@@ -1,28 +1,45 @@
-class LevenshteinAutomaton:
+class SparseLevenshteinAutomaton:
     def __init__(self, string, n):
         self.string = string
         self.max_edits = n
-        
+
     def start(self):
-    	# returns the start state
-        return range(len(self.string)+1)
+        return (range(self.max_edits+1), range(self.max_edits+1))
 
-    def step(self, state, c):
-    	# returns the next state given a state and a character
-        new_state = [state[0]+1]
-        for i in range(len(state)-1):
+    def step(self, indices, values, c):
+        if indices and indices[0] == 0 and values[0] < self.max_edits:
+            new_indices = [0]
+            new_values = [values[0] + 1]
+        else:
+            new_indices = []
+            new_values = []
+
+        for j,i in enumerate(indices):
+            if i == len(self.string): break
             cost = 0 if self.string[i] == c else 1
-            new_state.append(min(new_state[i]+1, state[i]+cost, state[i+1]+1))
-        return [min(x,self.max_edits+1) for x in new_state]
+            val = values[j] + cost
+            if new_indices and new_indices[-1] == i:
+                val = min(val, new_values[-1] + 1)
+            if j+1 < len(indices) and indices[j+1] == i+1:
+                val = min(val, values[j+1] + 1)
+            if val <= self.max_edits:
+                new_indices.append(i+1)
+                new_values.append(val)
 
-    def is_match(self, state):
-        return state[-1] <= self.max_edits
+        return (new_indices, new_values)
 
-    def can_match(self, state):
-        return min(state) <= self.max_edits
+    def is_match(self, indices):
+        return bool(indices) and indices[-1] == len(self.string)
 
-    def transitions(self, state):
-        return set(c for (i,c) in enumerate(self.string) if state[i] <= self.max_edits) 
+    def can_match(self, indices):
+        return bool(indices)
+
+    def transitions(self, indices):
+        return set(self.string[i] for i in indices if i < len(self.string))
+
+
+
+# use the automaton to build a DFA
 
 counter = [0] # list is a hack for mutable lexical scoping
 states = {}
@@ -31,75 +48,53 @@ matching = []
 words = []
 dic = open("american-english", 'r')
 dicWords = dic.read().splitlines() 
-interWords = []
 
 #string = "snake"
 string = input("Enter a word: ")
-lev = LevenshteinAutomaton(string, 1)
+lev = SparseLevenshteinAutomaton(string, 1)
 
 def explore(state):
-    key = tuple(state) # lists can't be hashed in Python so convert to a tuple
+    key = (tuple(state[0]),tuple(state[1])) # lists can't be hashed in Python because they are mutable, so convert to a tuple
     if key in states: return states[key]
     i = counter[0]
     counter[0] += 1
     states[key] = i
-    if lev.is_match(state): 
-        matching.append(i)
-    for c in lev.transitions(state) | set(['*']):
-        newstate = lev.step(state, c)
+    transitions.append([])
+    if lev.is_match(state[0]): matching.append(i)
+    for c in lev.transitions(state[0]) | set(['*']):
+        newstate = lev.step(state[0], state[1], c)
         j = explore(newstate)
-        transitions.append((i, j, c))
+        transitions[i].append((j, c))
     return i
-
-
-def check_asterisk(word):
-    for c in word:
-        if c == "*":
-            return True
-    return False
-
-def findAllWordsUtil(source, destination, visited, word):
-
-    visited[source] = True
-
-    if source == destination and check_asterisk(word) == True:
-        words.append(word)
-
-    else:
-        for i in transitions:
-            if(i[0] == source):
-                if visited[i[1]] == False:
-                    word = word + i[2]
-                    findAllWordsUtil(i[1], destination, visited, word)
-                    word = word[: len(word) - 1]
-    visited[source] = False
-
-def findAllWords(source = 0):
-    visited = [False] * (len(transitions))
-    word = ""
-    for s in matching:
-        findAllWordsUtil(source, s, visited, word)
 
 explore(lev.start())
 
+def sortChar(elem):
+    return elem[1]
 
+for i in range(len(transitions)):
+    transitions[i].sort(key = sortChar, reverse = True)
 
-def leveinsthein_comparation(word1, word2):
-    if(len(word1) != len(word2)):
-        return False
-    for i in range(len(word1)):
-        if word1[i] == word2[i] or word2[i] == "*":
-            continue
-        else:
-            return False
-    return True
+def levauto(word):
+    current_state = 0
+    if(abs(len(word) - len(string)) > 1):
+        return
 
-def comp():
+    for c in word:
+        for i in transitions[current_state]:
+            if(i[1] == c or i[1] == '*'):
+                current_state = i[0]
+                break
+                
+
+    for m in matching:
+        if current_state == m:
+            words.append(word)
+            break
+
+def levcomp():
     for i in dicWords:
-        for j in words:
-                if interWords.count(i) == 0 and leveinsthein_comparation(i, j) == True:
-                    interWords.append(i)
+        levauto(i)
 
-findAllWords()
-comp()
-print(interWords)
+levcomp()
+print(words)
